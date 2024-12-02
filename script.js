51ecf100a57d80dde98b938477925f9f6822c29d
rehanmansoor91 <rehanmansoor786786@gmail.com>
2024-12-02 19:05:06 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById("hamburger");
+const navlist = document.getElementById("navlist");
+
+hamburger.addEventListener("click", function () {
+
+    navlist.classList.toggle("navlist-active");
+
+})
+
+
